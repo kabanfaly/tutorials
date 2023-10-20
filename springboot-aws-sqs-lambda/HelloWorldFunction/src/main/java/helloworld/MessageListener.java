d333eb26cec2cfeef7331c1fe7cfcb79d3129184
N'faly Kaba <nfaly.kaba@cummins.com>
2023-10-20 10:58:23 -0400

    Message listener

--- springboot-aws-sqs-lambda/HelloWorldFunction/src/main/java/helloworld/MessageListener.java
+++ springboot-aws-sqs-lambda/HelloWorldFunction/src/main/java/helloworld/MessageListener.java
@@ -3,15 +3,14 @@ package helloworld;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class MessageListener {
     @SqsListener(value = "mySqsQueue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void receiveStringMessage(final String message, @Header("SenderId") String senderId) {
-        log.info("message received {} {}",senderId,message);
+    public void receiveStringMessage(final String message) {
+        log.info("message received {}",message);
     }
 
 }
